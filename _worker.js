4010cdb8261d6f8a72956c0818cf3da8ca91b2f2
44521 <132721007+44521@users.noreply.github.com>
2023-08-19 23:07:21 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,6 @@
 export default {
     async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        // 修改下方的example.com为自己的节点ip/域名
-        url.hostname="nl2-2.deploy.sbs:10568";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
+
       return env.ASSETS.fetch(request);
     }
   };
